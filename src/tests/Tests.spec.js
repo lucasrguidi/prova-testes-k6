75e2b5d9258a564918ab8be43e5ef6755c19eae1
Lucas Guidi <lucasrguidi@gmail.com>
2024-12-05 16:31:48 -0300

    first commit

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,22 +1,27 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Rate, Trend } from 'k6/metrics';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-import http from 'k6/http';
-import { check } from 'k6';
-import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getCatFactsDuration = new Trend('get_cat_facts_duration', true);
+export const rateStatusCodeOk = new Rate('rate_status_code_ok');
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
+const BASE_URL = 'https://catfact.ninja/fact';
+const STATUS_OK = 200;
+
+export let options = {
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '1m', target: 10 },
+    { duration: '2m', target: 100 },
+    { duration: '1m', target: 200 },
+    { duration: '1m', target: 300 }
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<5700'],
+    rate_status_code_ok: ['rate>0.95'],
+    http_req_failed: ['rate<0.12']
+  }
 };
 
 export function handleSummary(data) {
@@ -27,23 +32,16 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const res = http.get(BASE_URL);
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  getCatFactsDuration.add(res.timings.duration);
 
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
+  rateStatusCodeOk.add(res.status === STATUS_OK);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'Status is 200': r => r.status === STATUS_OK,
+    'Returned a fact': r => r.json().fact && r.json().fact.length > 0
   });
+
+  sleep(1);
 }
